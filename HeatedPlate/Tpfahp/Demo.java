8f224aa3901b0f80f039729020455efded615a99
ngaddame@hawk.iit.edu <ngaddame@hawk.iit.edu>
2014-09-12 13:41:54 -0400

    Eclipse Java project is created for Heated Plate project and added packages add classes required. However the classes are just empty and need to add code later.

---
+++ HeatedPlate/Tpfahp/Demo.java
@@ -0,0 +1,10 @@
+package Tpfahp;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Demo of the program from the package Tpfahp");
+	}
+
+}
