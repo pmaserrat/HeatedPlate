4b67b71786d9bcafae944c0e4fda2a4deccfc6d2
software-engineering <ubuntu@engineering.(none)>
2014-09-20 12:39:37 -0400

    updated testPackage to write to different results files

--- testing/testPackage.sh
+++ testing/testPackage.sh
@@ -2,14 +2,14 @@
 # run: testPackage.sh [filename] [package to run]
 # CSV file should provide parameters in the order d,l,r,t,b on each line
 	
-touch test.txt
+touch $1.txt
 
 while read p
 do
 	IFS=','
 	read -a myParams <<< "${p}"
-	java $2 -d ${myParams[0]} -l ${myParams[1]} -r ${myParams[2]} -t ${myParams[3]} -b ${myParams[4]} >> test.txt
+	java $2 -d ${myParams[0]} -l ${myParams[1]} -r ${myParams[2]} -t ${myParams[3]} -b ${myParams[4]} >> $1.txt
 
 done <$1
 	
-	echo -e "Tests complete. Output written to test.txt"
+	echo -e "Tests complete. Output written to $1.txt"
