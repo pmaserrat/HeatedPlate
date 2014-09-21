062aa91b3072a27cc02296b04d2622123a460f8c
Tyler Moore <moore.tyler.n@gmail.com>
2014-09-21 00:04:08 -0400

    updated testing files

--- testing/testPackage.sh
+++ testing/testPackage.sh
@@ -8,8 +8,10 @@ while read p
 do
 	IFS=','
 	read -a myParams <<< "${p}"
-	java $2 -d ${myParams[0]} -l ${myParams[1]} -r ${myParams[2]} -t ${myParams[3]} -b ${myParams[4]} >> $1.txt
-
+	for i in {1..1000}
+	do
+		java $2 -d ${myParams[0]} -l ${myParams[1]} -r ${myParams[2]} -t ${myParams[3]} -b ${myParams[4]} >> $1.txt
+	done
 done <$1
 	
 	echo -e "Tests complete. Output written to $1.txt"
